3198bc7bece9e030a3e5b9491a4e3e0ef043acf3
DaleUnuniUm <martin.romuald2@hotmail.fr>
2021-01-26 14:18:47 +0100

    Versions 0.2
    Mise en place de la configuration MVC

--- src/main/java/com/openclassrooms/paymybuddyapi/PaymybuddyapiApplication.java
+++ src/main/java/com/openclassrooms/paymybuddyapi/PaymybuddyapiApplication.java
@@ -3,6 +3,7 @@ package com.openclassrooms.paymybuddyapi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class PaymybuddyapiApplication implements CommandLineRunner {
